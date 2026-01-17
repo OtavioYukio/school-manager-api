e081a34e98f5615355a7644d7320b9473423c0c8
OtavioYukio <yukiootavio@gmail.com>
2026-01-17 14:36:12 -0300

    feat: adicionar metodo authenticationManager em SecurityConfig

--- src/main/java/com/otavioyukio/school_manager_api/auth/SecurityConfig.java
+++ src/main/java/com/otavioyukio/school_manager_api/auth/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.otavioyukio.school_manager_api.auth;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -39,6 +41,12 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager
+            (AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }

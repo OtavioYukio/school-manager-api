2820673fcbc9b252a7ee550e4ea0668373e5e4d9
OtavioYukio <yukiootavio@gmail.com>
2026-01-17 13:48:34 -0300

    feat: adicionar metodo generateExpirationDate em TokenService

--- src/main/java/com/otavioyukio/school_manager_api/auth/TokenService.java
+++ src/main/java/com/otavioyukio/school_manager_api/auth/TokenService.java
@@ -3,8 +3,16 @@ package com.otavioyukio.school_manager_api.auth;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 @Service
 public class TokenService {
     @Value("${spring.jwt.secret}")
     private String secret;
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-3"));
+    }
 }

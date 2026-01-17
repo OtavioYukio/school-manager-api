fb3fd9f03deb22fdef892f613bb9d69fbf46ebaf
OtavioYukio <yukiootavio@gmail.com>
2026-01-17 13:23:33 -0300

    feat: criar metodo existsByEmail em UserRepository

--- src/main/java/com/otavioyukio/school_manager_api/user/UserRepository.java
+++ src/main/java/com/otavioyukio/school_manager_api/user/UserRepository.java
@@ -3,4 +3,5 @@ package com.otavioyukio.school_manager_api.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
 }

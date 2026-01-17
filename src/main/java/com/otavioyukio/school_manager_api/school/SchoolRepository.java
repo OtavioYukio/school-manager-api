a94fdeec001bdacb4d85aa179e322320591624b2
OtavioYukio <yukiootavio@gmail.com>
2026-01-16 22:16:03 -0300

    feat: criar interface SchoolRepository subclasse de JpaRepository

---
+++ src/main/java/com/otavioyukio/school_manager_api/school/SchoolRepository.java
@@ -0,0 +1,8 @@
+package com.otavioyukio.school_manager_api.school;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface SchoolRepository extends JpaRepository<School, Long> {
+
+     
+}

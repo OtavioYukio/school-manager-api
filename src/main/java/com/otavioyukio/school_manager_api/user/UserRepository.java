8b895e3c5b9643a17dd9eabfc83337b718fa47b6
OtavioYukio <yukiootavio@gmail.com>
2026-01-17 14:02:56 -0300

    feat: adicionar metodo findByEmail em UserRepository

--- src/main/java/com/otavioyukio/school_manager_api/user/UserRepository.java
+++ src/main/java/com/otavioyukio/school_manager_api/user/UserRepository.java
@@ -2,6 +2,9 @@ package com.otavioyukio.school_manager_api.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }

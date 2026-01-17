c2e8af427285a4a2c10d966f6f6686c05099f67a
OtavioYukio <yukiootavio@gmail.com>
2026-01-16 22:06:26 -0300

    feat: criar entidade School

---
+++ src/main/java/com/otavioyukio/school_manager_api/school/School.java
@@ -0,0 +1,37 @@
+package com.otavioyukio.school_manager_api.school;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "schools")
+@Getter
+@Setter
+@NoArgsConstructor
+class School {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public School(String name, LocalDateTime createdAt) {
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+}

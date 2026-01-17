3e9d35e0a93e1d303f4855ff3209c09d51104aa8
OtavioYukio <yukiootavio@gmail.com>
2026-01-17 13:11:37 -0300

    feat: adicionar validations no record UserRequestDTO

--- src/main/java/com/otavioyukio/school_manager_api/user/UserRequestDTO.java
+++ src/main/java/com/otavioyukio/school_manager_api/user/UserRequestDTO.java
@@ -1,10 +1,18 @@
 package com.otavioyukio.school_manager_api.user;
 
 import com.otavioyukio.school_manager_api.school.School;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UserRequestDTO(
+        @Email
+        @Size(max = 255)
         String email,
+        @NotBlank
+        @Size(min = 8, max = 255)
         String password,
+        @NotBlank
         School school
 ) {
 }

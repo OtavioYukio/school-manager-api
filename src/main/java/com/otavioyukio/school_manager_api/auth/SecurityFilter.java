82757fac553b3e652cd7f54d3062ceb9c78aaa01
OtavioYukio <yukiootavio@gmail.com>
2026-01-17 14:27:39 -0300

    feat: criar classe SecurityFilter

---
+++ src/main/java/com/otavioyukio/school_manager_api/auth/SecurityFilter.java
@@ -0,0 +1,53 @@
+package com.otavioyukio.school_manager_api.auth;
+
+import com.otavioyukio.school_manager_api.user.User;
+import com.otavioyukio.school_manager_api.user.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    TokenService tokenService;
+    UserService userService;
+
+    public SecurityFilter(TokenService tokenService, UserService userService) {
+        this.tokenService = tokenService;
+        this.userService = userService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String path = request.getServletPath();
+
+        if (path.equals("/auth/login") || path.equals("auth/register")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = this.recoverToken(request);
+        String login = tokenService.validateToken(token);
+
+        if (login != null) {
+            User user = userService.findUserByEmail(login);
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}

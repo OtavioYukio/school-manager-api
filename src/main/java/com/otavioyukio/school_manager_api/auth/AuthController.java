223e0e68eee5e5ff6b2a61c1e9c508a6086e833f
OtavioYukio <yukiootavio@gmail.com>
2026-01-17 15:38:30 -0300

    feat: criar classe AuthController com o metodo Post register

---
+++ src/main/java/com/otavioyukio/school_manager_api/auth/AuthController.java
@@ -0,0 +1,24 @@
+package com.otavioyukio.school_manager_api.auth;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponseDTO> register(@RequestBody @Valid RegisterRequestDTO requestDTO) {
+        RegisterResponseDTO responseDTO = authService.register(requestDTO);
+        return ResponseEntity.ok(responseDTO);
+    }
+}

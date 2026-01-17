ec35cf927f5874a5ac61564a8906ed33b1c44542
OtavioYukio <yukiootavio@gmail.com>
2026-01-17 15:35:14 -0300

    feat: criar record LoginRequestDTO

--- src/main/java/com/otavioyukio/school_manager_api/auth/LoginRequestDTO.java
+++ src/main/java/com/otavioyukio/school_manager_api/auth/LoginRequestDTO.java
@@ -1,7 +1,15 @@
 package com.otavioyukio.school_manager_api.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record LoginRequestDTO (
+        @Email
+        @Size(max = 255)
         String email,
+        @NotBlank
+        @Size(min = 8, max = 255)
         String password
 ){
 }

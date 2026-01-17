f18ba7add7235eabfdfbfb011516a8f58f736063
OtavioYukio <yukiootavio@gmail.com>
2026-01-17 15:13:19 -0300

    feat: criar classe AuthService e adicionar metodo register

---
+++ src/main/java/com/otavioyukio/school_manager_api/auth/AuthService.java
@@ -0,0 +1,43 @@
+package com.otavioyukio.school_manager_api.auth;
+
+import com.otavioyukio.school_manager_api.school.School;
+import com.otavioyukio.school_manager_api.school.SchoolService;
+import com.otavioyukio.school_manager_api.user.Role;
+import com.otavioyukio.school_manager_api.user.User;
+import com.otavioyukio.school_manager_api.user.UserService;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+class AuthService {
+    private final UserService userService;
+    private final TokenService tokenService;
+    private final PasswordEncoder passwordEncoder;
+
+    AuthService(UserService userService, TokenService tokenService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.tokenService = tokenService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    RegisterResponseDTO register(RegisterRequestDTO registerRequestDTO) {
+        School newSchool = new School(registerRequestDTO.name());
+        User newUser = new User(
+                registerRequestDTO.email(),
+                passwordEncoder.encode(registerRequestDTO.password()),
+                newSchool,
+                Role.ADMIN
+        );
+        userService.createUser(newUser);
+
+        String token = tokenService.generateToken(newUser);
+
+        return new RegisterResponseDTO(
+                token,
+                newUser.getEmail(),
+                newUser.getSchool().getName(),
+                newUser.getCreatedAt()
+        );
+    }
+}

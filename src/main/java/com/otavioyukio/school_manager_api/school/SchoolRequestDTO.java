20e76bd679ee2ae603c23c3b6de6592c6b7ebad1
OtavioYukio <yukiootavio@gmail.com>
2026-01-16 22:38:31 -0300

    feat: adicionar validação do campo name em SchoolRequestDTO

--- src/main/java/com/otavioyukio/school_manager_api/school/SchoolRequestDTO.java
+++ src/main/java/com/otavioyukio/school_manager_api/school/SchoolRequestDTO.java
@@ -1,6 +1,11 @@
 package com.otavioyukio.school_manager_api.school;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record SchoolRequestDTO(
+    @NotBlank
+    @Size(max = 150)
     String name
 ) {
     

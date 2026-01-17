73530db6bc0398a3f7f94c5d83c29767727ec795
OtavioYukio <yukiootavio@gmail.com>
2026-01-17 13:54:53 -0300

    feat: adicionar metodo generateToken em TokenService

--- src/main/java/com/otavioyukio/school_manager_api/auth/TokenService.java
+++ src/main/java/com/otavioyukio/school_manager_api/auth/TokenService.java
@@ -1,5 +1,9 @@
 package com.otavioyukio.school_manager_api.auth;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.otavioyukio.school_manager_api.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +12,25 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
 @Service
-public class TokenService {
+class TokenService {
     @Value("${spring.jwt.secret}")
     private String secret;
+
+    String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.create()
+                    .withIssuer("school-manager-api")
+                    .withSubject(user.getEmail())
+                    .withClaim("userId", user.getId())
+                    .withClaim("schoolId", user.getSchool().getId())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException ex) {
+            throw new RuntimeException("Error while authenticating");
+        }
+    }
 
     private Instant generateExpirationDate() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-3"));

7c29e1447f0321567d6f685d13afebd98f3249d0
OtavioYukio <yukiootavio@gmail.com>
2026-01-17 13:42:18 -0300

    feat: criar classe GlobalExceptionHandler para personalizar exceções globalmente

---
+++ src/main/java/com/otavioyukio/school_manager_api/commons/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.otavioyukio.school_manager_api.commons.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public final ResponseEntity<Object>
+    handleObjectNotFoundException(ObjectNotFoundException ex) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("error", ex.getMessage());
+        return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
+    }
+}

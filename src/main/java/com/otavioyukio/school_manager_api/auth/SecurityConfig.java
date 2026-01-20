3803bb3bae00f451146f90362c1615e5eb8d7830
OtavioYukio <yukiootavio@gmail.com>
2026-01-19 22:34:45 -0300

    feat: adicionar nova url no CORS config

--- src/main/java/com/otavioyukio/school_manager_api/auth/SecurityConfig.java
+++ src/main/java/com/otavioyukio/school_manager_api/auth/SecurityConfig.java
@@ -56,7 +56,8 @@ public class SecurityConfig {
                 "http://localhost:3000",
                 "http://127.0.0.1:3000",
                 "http://localhost:5500",
-                "http://127.0.0.1:5500"
+                "http://127.0.0.1:5500",
+                "https://school-manager-frontend.onrender.com"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));

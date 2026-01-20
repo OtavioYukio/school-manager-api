1166afb6a9c92fd5c8f2619b99a810d7751e8652
OtavioYukio <yukiootavio@gmail.com>
2026-01-19 21:27:38 -0300

    feat: configurando api para deploy

--- src/main/java/com/otavioyukio/school_manager_api/auth/SecurityConfig.java
+++ src/main/java/com/otavioyukio/school_manager_api/auth/SecurityConfig.java
@@ -53,6 +53,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "http://127.0.0.1:3000",
                 "http://localhost:5500",
                 "http://127.0.0.1:5500"
         ));
